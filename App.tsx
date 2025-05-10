9f5bce2e4ff7b2be16988398f95bba8cf092bb8a
maicontech <maicon.ribeiro13@etec.sp.gov.br>
2025-05-09 22:34:52 -0300

    finalizado

--- App.tsx
+++ App.tsx
@@ -1,29 +1,55 @@
-
+import{useState} from 'react';
 import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
-
+ 
 export default function App() {
+const[peso, setPeso] = useState('');
+const[altura, setAltura] = useState('');
 
+const[resultado, setResultado] = useState('');
+ 
   function calcularImc(){
-    alert("Calculo do IMC");
-  }
+    let msg = "";
+    let alturaEmMetro = parseFloat(altura)/100;
+    let result = parseFloat(peso)/(alturaEmMetro * alturaEmMetro);
 
+    if(result < 18.5){
+      msg = "Abaixo do peso"
+    } else if(result <= 24.99) {
+      msg = "Peso ideal"
+    } else if(result <= 29.99) {
+      msg = "Levemente acima do peso"
+    } else if(result <= 34.99) {
+      msg = "Obesidade grau I"
+    } else if(result <= 39.99) {
+      msg = "Obesidade grau II"
+    } else if(result > 40) {
+      msg = "Obesidade grau III"
+    }
+
+    setResultado("Valor do IMC: "+result.toFixed(2)+"\n"+msg);
+  }
+ 
   return (
     <View style={styles.container}>
       <Text style={styles.titulo}> IMC </Text>
-      
+     
       <View style={styles.bloco}>
           <Text style={styles.label}>Peso</Text>
-          <TextInput 
+          <TextInput
             style={styles.input}
             keyboardType='numeric'
+            value={peso}
+            onChangeText={(valor)=>setPeso(valor)}
           />
       </View>
       <View style={styles.bloco}>
           <Text style={styles.label} >Altura</Text>
-          <TextInput 
+          <TextInput
             style={styles.input}
             keyboardType='numeric'
-            
+            value={altura}
+            onChangeText={(valor)=>setAltura(valor)}
+           
           />
       </View>
       <View style={styles.bloco}>
@@ -31,10 +57,13 @@ export default function App() {
               <Text style={styles.btnTxt}>Calcular</Text>
           </TouchableOpacity>
       </View>
+      <View style={styles.bloco}>
+        <Text style={styles.titulo}>{resultado}</Text>
+      </View>
     </View>
   );
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
